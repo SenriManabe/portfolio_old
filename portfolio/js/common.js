1bfdb6c3d77b93a96ec6cb560912a4643badd58d
SenriManabe <senri.dr@gmail.com>
2020-03-06 19:05:32 +0900

    ajaxにしたことによってバグっていた部分を修正

--- portfolio/js/common.js
+++ portfolio/js/common.js
@@ -1,20 +1,4 @@
-var form = '.js-form',
-	$form = $(form),
-	formBtn = '.js-form-btn',
-	$formBtn = $(formBtn),
-	confirm = '.js-confirm',
-	$confirm = $(confirm),
-	inputName = '.js-input-name',
-	$inputName = $(inputName),
-	inputMail = '.js-input-mail',
-	$inputMail = $(inputMail),
-	textArea = '.js-textarea',
-	$textArea = $(textArea),
-	inputAdd = '.js-input-add',
-	$inputAdd = $(inputAdd),
-	contents = '.js-contents',
-	$contents = $(contents),
-	kvSlider = '.js-kv-slider',
+var kvSlider = '.js-kv-slider',
 	$kvSlider = $(kvSlider),
 	navTrigger = '.js-nav-trigger',
 	$navTrigger = $(navTrigger),
@@ -22,54 +6,61 @@ var form = '.js-form',
 	$ajaxLink = $(ajaxLink),
 	ajaxChange = '.js-ajax-change',
 	$ajaxChange = $(ajaxChange),
-	wHeight = $contents.eq(0).outerHeight(),
 	liCount =  0;
 
 // SPのGnavi設定
-$navTrigger.on('click', function(event) {
-	$(this).toggleClass('is-active');
-	$(this).next().fadeToggle();
-});
+function menu(){
+	$navTrigger.on('click', function(event) {
+		$(this).toggleClass('is-active');
+		$(this).next().fadeToggle();
+	});
+}
 
 // fade slider設定
-$(function(){
-	var count = 0,
-		interval = 5000
-		length = ($kvSlider.children().length) - 1;
+function slider(){
+	$(function(){
+		var count = 0,
+			interval = 5000
+			length = ($kvSlider.children().length) - 1;
 
-	var timer = setInterval(show, interval);
-	$('.js-count-stop').hover(function(){
-		clearInterval(timer);
-    }, function(){
-		timer = setInterval(show, interval);
-    })
-	function show(){
-		$kvSlider.children().eq(count).removeClass('is-active');
-		$kvSlider.children().eq(count).next().addClass('is-active');
-		if (count == length) {
-			count = 0;
-			$kvSlider.children().eq(count).addClass('is-active');
-		} else {
-			count++;
+		var timer = setInterval(show, interval);
+		$('.js-count-stop').hover(function(){
+			clearInterval(timer);
+	    }, function(){
+			timer = setInterval(show, interval);
+	    })
+		function show(){
+			$kvSlider.children().eq(count).removeClass('is-active');
+			$kvSlider.children().eq(count).next().addClass('is-active');
+			if (count == length) {
+				count = 0;
+				$kvSlider.children().eq(count).addClass('is-active');
+			} else {
+				count++;
+			}
 		}
-	}
-});
+	});
+}
 
 // view height設定
-var vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-// window resize
-window.addEventListener('resize', () => {
-    vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
+function vhConf(){
+	var vh = window.innerHeight * 0.01;
+	document.documentElement.style.setProperty('--vh', `${vh}px`);
+	// window resize
+	window.addEventListener('resize', () => {
+	    vh = window.innerHeight * 0.01;
+	    document.documentElement.style.setProperty('--vh', `${vh}px`);
+	});
+}
 
-var lastpage = "index.html",
-	ref = document.referrer;
+var ref = document.referrer;
 
 $(window).on('popstate', function(event){
 	$ajaxChange.fadeOut(300, function() {
 		getPage(ref);
+		if ($kvSlider.length == 1) {
+			slider();
+		}
 	});
 });
 
@@ -79,6 +70,9 @@ $(document).on('click', ajaxLink, function(event) {
 
 	$ajaxChange.fadeOut(300, function() {
 		getPage(link);
+		if ($kvSlider.length == 1) {
+			slider();
+		}
 	});
 });
 
@@ -96,3 +90,10 @@ function getPage(elm){
 		}
 	});
 }
+
+/** 初回ロード時にコールバックを実行 **/
+menu();
+if ($kvSlider.length == 1) {
+	slider();
+}
+vhConf();
