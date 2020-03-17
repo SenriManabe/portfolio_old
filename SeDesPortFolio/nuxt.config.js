4b2498b075985523a8d642493d0ae8e58c54fd9f
SenriManabe <senri.dr@gmail.com>
2020-03-17 17:47:40 +0900

    github pages用の設定を削除

--- SeDesPortFolio/nuxt.config.js
+++ SeDesPortFolio/nuxt.config.js
@@ -1,12 +1,4 @@
-// `DEPLOY_ENV` が `GH_PAGES` の場合のみ `router.base = '/<repository-name>/'` を追加する
-const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
-  router: {
-    base: '/SeDesPortFolio/'
-  }
-} : {}
-
 export default {
-  ...routerBase,
   mode: 'spa',
   /*
   ** Headers of the page
